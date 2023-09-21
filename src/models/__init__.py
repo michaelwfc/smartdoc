ee510790ab49299aff8e2fe80dd92ad04834f0f5
michaelwfc <wang_feicheng@163.com>
2023-09-22 05:41:16 +0800

    build a new repo of smartdoc

--- src/models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-@author: michaelwfc
-@version: 1.0.0
-@license: Apache Licence
-@file: __init__.py.py
-@time: 2023/8/30 22:38
-"""
--- src/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-@author: michaelwfc
-@version: 1.0.0
-@license: Apache Licence
-@file: __init__.py
-@time: 2023/9/3 10:05
-"""
